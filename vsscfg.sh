ffb20e0592021f3a863203378ca3adc4588fb497
Diogo Marques <dgm@fcul-vm.(none)>
2013-10-13 01:13:50 +0100

    done?

--- vsscfg.sh
+++ vsscfg.sh
@@ -10,6 +10,7 @@
 # vsscfg.sh
 
 fname=config.vss
+validate=0
 disk=0
 did=0
 dtamanho=0
@@ -22,6 +23,7 @@ cfname=0
 ###########FUNCOES NECESSÁRIAS A EXECUCAO DO SCRIPT##########
 
 validate(){
+checkDuplicateNames
 checkDuplicates
 checkDeclarations
 checkDiskSectors
@@ -29,27 +31,96 @@ checkDiskImages
 }
 
 addDisk(){
-echo "disk $did $dtamanho" >> $fname
+echo "disc $did $dtamanho" >> $fname
 }
 
 addConf(){
 echo "raidvss $cnome $cid1 $cid2 $cfname" >> $fname
 }
 
+checkDuplicateNames(){
+ORIGIFS=$IFS
+IFS=`echo -en "\n\b"`
+for i in `cat $fname | awk -F" " '{print $2}'`
+do
+	if [ `cat $fname | awk -F" " '{print $2}' | grep $i | wc -l` -gt 1 ]
+		then echo "Discos ou configuracoes com o mesmo nome: $i"
+	fi
+done
+
+IFS=$ORIGIFS
+}
+
 checkDiskSectors(){
-echo checkDiskSectorssssssssss
+
+ORIGIFS=$IFS
+IFS=`echo -en "\n\b"`
+
+for i in `cat $fname | grep raidvss`
+do
+	d1=`echo $i | awk -F" " '{print $3}'`
+	d2=`echo $i | awk -F" " '{print $4}'`
+	if [ `cat $fname | grep "disc $d1" | awk -F" " '{print $3}'` -ne `cat $fname | grep "disc $d2" | awk -F" " '{print $3}'` ]
+		then
+			echo "Discos sem o mesmo numero de sectores na configuracao : $i"
+	fi
+done
+
+IFS=$ORIGIFS
 }
 
 checkDiskImages(){
-echo checkDiskImages
+ORIGIFS=$IFS
+IFS=`echo -en "\n\b"`
+for i in `cat config.vss | grep raidvss`
+do
+	filedisc=`echo $i | awk -F" " '{print $5}'`
+	d1=`echo $i | awk -F" " '{print $3}'`
+	d2=`echo $i | awk -F" " '{print $4}'`
+	if [ -f $filedisc ]
+	then
+		if [ `cat config.vss | grep "disc $d1" | awk -F" " '{print $3}'` -ne `du -b $filedisc | awk -F" " '{print $1}'` ] || [ `cat config.vss | grep "disc $d2" | awk -F" " '{print $3}'` -ne `du -b $filedisc | awk -F" " '{print $1}'` ]
+			then
+				echo "Imagem nao tem o mesmo numero de sectores: $i"
+		fi
+	else echo "Imagem nao existe: $i"
+	fi
+done
+IFS=$ORIGIFS
 }
 
 checkDuplicates(){
-echo checkDuplicates
+ORIGIFS=$IFS
+IFS=`echo -en "\n\b"`
+
+for i in `cat $fname | grep raidvss`
+do
+	d1=`echo $i | awk -F" " '{print $3}'`
+	d2=`echo $i | awk -F" " '{print $4}'`
+	if [ $d1 = $d2 ]
+		then
+			echo "Discos duplicados na configuracao: $i"
+	fi
+done
+
+IFS=$ORIGIFS
 }
 
 checkDeclarations(){
-echo checkDeclarations
+ORIGIFS=$IFS
+IFS=`echo -en "\n\b"`
+
+for i in `cat $fname | grep raidvss`
+do
+	d1=`echo $i | awk -F" " '{print $3}'`
+	d2=`echo $i | awk -F" " '{print $4}'`
+	if [ -z `cat $fname | grep "disc $d1"` ] || [ -z `cat $fname | grep "disc $d2"` ]
+		then
+			echo "Configuracao com discos nao declarados: $i"
+	fi
+done
+
+IFS=$ORIGIFS
 }
 
 #MAIN
@@ -71,7 +142,7 @@ then
 			then conf=1 && cnome=$2 && cid1=$3 && cid2=$4 && cfname=$5 && shift 5
 			else conf=1 && echo "Insira nome da configuracao:" && read cnome && echo "Disk ID 1:" && read cid1 && echo "Disk ID 2:" && read cid2 && echo "Nome do ficheiro de imagem:" && read cfname && shift
 			fi;;
-		-v) validate;;
+		-v) validate && validate=1;;
 		esac
 	done
 fi
@@ -84,6 +155,6 @@ if [ $conf -eq 1 ]
 	then addConf
 fi
 
-if [ $conf -eq 0 ] && [ $disk -eq 0 ]
+if [ $conf -eq 0 ] && [ $disk -eq 0 ] && [ $validate -eq 0 ]
 	then cat $fname 
 fi
