43c4403fe1d633b712206fdc6cab76152930cb63
Diogo Marques <dmarkes@gmail.com>
2013-10-23 14:17:12 +0100

    tretas

--- src/diskutils.c
+++ src/diskutils.c
@@ -6,3 +6,55 @@
 #include "ipc.h"
 #include "disk.h"
 
+
+/**
+ * Escolhe o próximo pedido a tratar pelo disco na politica LOOK
+ * Invocado por select_request
+ * Retorna o indice do pedido selecionado
+ */
+int select_request_LOOK(request_t* pool,int out){
+
+}
+
+/**
+ * Escolhe o próximo pedido a tratar pelo disco na politica FCFS
+ * Invocado por select_request
+ * Retorna o indice do pedido selecionado
+ */
+int select_request_FCFS(request_t* pool,int out){
+
+}
+
+/**
+ * Copia o sector corrente (dado pela variável global csector) do
+ * disco (representado por disk) para o argumento sect.
+ * Se o sector corrente exceder a dimensão do disco, retorna 0 e
+ * posiciona o disco no sector 0. Caso contrário retorna 1
+ */
+int disk_read_sector(secdata_t* sect,void* disk){
+
+}
+
+/**
+ * Copia para sect o sector solicitado em request. Invoca em sequência
+ * as funções disk_seek e disk_read_sector para o fazer.
+ * Retorna 0 em caso de erro e 1 caso contrário.
+ */
+int move_and_read(request_t* request,secdata_t* sect,void* disk){
+
+}
+
+/**
+ * Imprime informação de utilização para o stderr
+ */
+void print_help(char* progname){
+
+}
+
+/**
+ * Imprime o conteúdo do sector para o stdout no formato <nº do sector>:<conteúdo>
+ */
+void put_sector(sector_t sector,secdata_t* secdata){
+	
+}
+
--- src/main.c
+++ src/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include "request.h"
 
-int main(){
-	request_t a;
-	sector_t z=100;
-	fill_request(&a,z);
-
-	printf("%d\n",a.sector);
-
-	if(a.status==empty)
-		printf("empty\n");
-	else
-		printf("notempty\n");
-
+main(){
+	sector_t b=300;
+	sector_t z=23;
+	sector_t b2=599;
+	sector_t g=2;
+	request_t* pool1;
+	fill_request(pool1,b);
+	fill_request(pool1+1,z);
+	request_t* pool2;
+	fill_request(pool2,g);
 
 
+
+
+	printf("%d\n",(*pool1).sector);
+	printf("%d\n",(*(pool1+1)).sector);
+	printf("%d\n",(*pool2).sector);
+
+
+	// if((pool+1)->status==empty)
+	// 	printf("empty\n");
+	// if((pool+1)->status==waiting)
+	// 	printf("waiting\n");
 }
